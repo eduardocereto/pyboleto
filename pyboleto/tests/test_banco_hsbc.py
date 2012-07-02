e014cf2a362624306b25ba085cd3d29330954273
Davi Campos <arlocm@gmail.com>
2012-07-02 13:14:15 -0300

    Adding more tests do Boleto Hsbc

--- pyboleto/tests/test_banco_hsbc.py
+++ pyboleto/tests/test_banco_hsbc.py
@@ -7,32 +7,34 @@ from pyboleto.bank.hsbc import BoletoHsbc
 class TestBancoHsbc(unittest.TestCase):
     def setUp(self):
         d = BoletoHsbc()
-        d.carteira = '06'
-        d.agencia_cedente = '1172'
-        d.conta_cedente = '8351202'
-        d.data_vencimento = datetime.date(2011, 2, 5)
-        d.data_documento = datetime.date(2011, 1, 18)
-        d.data_processamento = datetime.date(2011, 1, 18)
-        d.valor_documento = 8280.00
-        d.nosso_numero = '2125525'
-        d.numero_documento = '2125525'
+        d.agencia_cedente = '1172-0'
+        d.conta_cedente = '3903036'
+        d.data_vencimento = datetime.date(2009, 05, 25)
+        d.data_documento = datetime.date(2009, 05, 25)
+        d.data_processamento = datetime.date(2009, 05, 25)
+        d.valor_documento = 35.00
+        #d.nosso_numero = '2125525'
+        d.numero_documento = '0100010103120'
         self.dados = d
 
     def test_linha_digitavel(self):
         self.assertEqual(self.dados.linha_digitavel, 
-            '23790.27804 60000.212559 25003.923205 4 48690000828000'
+            '39993.90309 36010.001018 03120.145929 3 42480000003500'
         )
     
     def test_codigo_de_barras(self):
         self.assertEqual(self.dados.barcode, 
-            '23794486900008280000278060000212552500392320'
+            '39993424800000035003903036010001010312014592'
         )
 
     def test_agencia(self):
         self.assertEqual(self.dados.agencia_cedente, '1172-0')
 
     def test_conta(self):
-        self.assertEqual(self.dados.conta_cedente, '8351202')
+        self.assertEqual(self.dados.conta_cedente, '3903036')
+
+    def test_nosso_numero(self):
+        self.assertEqual(self.dados.nosso_numero, '0100010103120947')
 
 if __name__ == '__main__':
     unittest.main()
