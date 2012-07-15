2ce9edf56fc87c41d71d4327caccc7e19e28f41f
Eduardo Cereto Carvalho <eduardocereto@gmail.com>
2012-07-14 22:10:49 -0700

    v2.8 Improve Py3 build

--- pyboleto/__init__.py
+++ pyboleto/__init__.py
@@ -1,8 +1,1 @@
-VERSION = (0, 2, 5)
-__version__ = '.'.join(map(str, VERSION))
-
-__all__ = [
-    'data',
-    'pdf',
-    'bank',
-]
+__version__ = '0.2.8'
--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 import sys
+import os
+import re
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def get_version(package):
+    """Return package version as listed in `__version__` in `__init__.py`."""
+    init_py = open(os.path.join(os.path.dirname(__file__),
+                                package, '__init__.py'),
+                   'r').read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]",
+                     init_py, re.MULTILINE
+    ).group(1)
 
 
 extra = {}
@@ -8,6 +24,9 @@ if sys.version_info >= (3,):
     extra['use_2to3'] = True
     #extra['convert_2to3_doctests'] = ['src/your/module/README.txt']
     #extra['use_2to3_fixers'] = ['your.fixers']
+    extra['install_requires'] = [
+        'distribute',
+    ]
     extra['tests_require'] = [
         'pep8>=0.6.1',
         'pep8<1.3',
@@ -24,7 +43,7 @@ else:
 
 setup(
     name='pyboleto',
-    version='0.2.5',
+    version=get_version('pyboleto'),
     author='Eduardo Cereto Carvalho',
     author_email='eduardocereto@gmail.com',
     url='https://github.com/eduardocereto/pyboleto',
@@ -39,7 +58,7 @@ setup(
     ],
     license='BSD',
     description='Python Library to create "boletos de cobrança bancária" for several Brazilian banks',
-    long_description=open('README.rst', 'r').read(),
+    long_description=read('README.rst'),
     download_url='http://pypi.python.org/pypi/pyboleto',
     scripts=['bin/pyboleto_sample.py'],
     classifiers=[
