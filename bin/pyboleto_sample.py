5844f73033bf8e92575a4540e7eb9e9da1a244fe
Eduardo Cereto Carvalho <eduardocereto@gmail.com>
2012-07-17 01:29:24 -0700

    Update pyboleto_sample.py
    Esse arquivo estava sendo ignorado pelo git. Acho que perdi varios
    commits nele

--- bin/pyboleto_sample.py
+++ bin/pyboleto_sample.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+#!/Users/dudus/Work/pyboleto/venv/bin/python
 # -*- coding: utf-8 -*-
 import pyboleto
 from pyboleto.bank.real import BoletoReal
 from pyboleto.bank.bradesco import BoletoBradesco
 from pyboleto.bank.caixa import BoletoCaixa
 from pyboleto.bank.bancodobrasil import BoletoBB
+from pyboleto.bank.santander import BoletoSantander
 from pyboleto.pdf import BoletoPDF
 import datetime
 
@@ -150,6 +151,50 @@ def print_bradesco():
     boleto.save()
 
 
+def print_santander():
+    listaDadosSantander = []
+    for i in range(2):
+        d = BoletoSantander()
+        d.agencia_cedente = '1333'
+        d.conta_cedente = '0707077'
+        d.data_vencimento = datetime.date(2012, 7, 22)
+        d.data_documento = datetime.date(2012, 7, 17)
+        d.data_processamento = datetime.date(2012, 7, 17)
+        d.valor_documento = 2952.95
+        d.nosso_numero = '1234567'
+        d.numero_documento = '12345'
+        d.ios = '0'
+
+        d.cedente = 'Empresa ACME LTDA'
+        d.cedente_documento = "102.323.777-01"
+        d.cedente_endereco = "Rua Acme, 123 - Centro - Sao Paulo/SP - CEP: 12345-678"
+
+        d.instrucoes = [
+            "- Linha 1",
+            "- Sr Caixa, cobrar multa de 2% após o vencimento",
+            "- Receber até 10 dias após o vencimento",
+            ]
+        d.demonstrativo = [
+            "- Serviço Teste R$ 5,00",
+            "- Total R$ 5,00",
+            ]
+        d.valor_documento = 255.00
+
+        d.sacado = [
+            "Cliente Teste %d" % (i + 1),
+            "Rua Desconhecida, 00/0000 - Não Sei - Cidade - Cep. 00000-000",
+            ""
+            ]
+        listaDadosSantander.append(d)
+
+    # Caixa Formato normal - uma pagina por folha A4
+    boleto = BoletoPDF('boleto-santander-formato-normal-teste.pdf')
+    for i in range(len(listaDadosSantander)):
+        boleto.drawBoleto(listaDadosSantander[i])
+        boleto.nextPage()
+    boleto.save()
+
+
 def print_caixa():
     listaDadosCaixa = []
     for i in range(2):
@@ -227,6 +272,9 @@ def print_all():
 
     print "Real"
     print_real()
+
+    print "Santander"
+    print_santander()
 
     print "----------------------------------"
     print "Ok"
